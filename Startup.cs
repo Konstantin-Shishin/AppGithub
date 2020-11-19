4a69923aa58800b394916e7403320777d67e7765
Konstantin-Shishin <shishinkostya2000@gmail.com>
2020-11-19 22:35:07 +0600

    add symbol

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace AppGithub
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Worlds!");
                 });
             });
         }
